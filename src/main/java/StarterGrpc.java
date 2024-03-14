5bfd1c0793522033dde8dd548081fc2452aad816
konstantinsergeev <andrewsergeev98@gmail.com>
2024-03-14 23:51:27 +0300

    .

--- src/main/java/StarterGrpc.java
+++ src/main/java/StarterGrpc.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class StarterGrpc {
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import seervice.GRPCService;
+
+import java.io.IOException;
+
+public class StarterGrpc {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("сервер run");
+        Server server= ServerBuilder
+                .forPort(8080)
+                .addService(new GRPCService())
+                .build();
+        server.start();
+
+        server.awaitTermination();
+    }
 }
--- src/main/java/seervice/GRPCService.java
+++ src/main/java/seervice/GRPCService.java
@@ -1,2 +1,18 @@
-package seervice;public class GRPCService {
+package seervice;
+import io.grpc.stub.StreamObserver;
+import org.example.grpc.GreetingServiceGrpc;
+import org.example.grpc.GreetingServiceOuterClass;
+
+public class GRPCService extends GreetingServiceGrpc.GreetingServiceImplBase{
+    @Override
+    public void greeting(GreetingServiceOuterClass.HelloRequest rq
+            , StreamObserver<GreetingServiceOuterClass.HelloResponse> rs){
+        GreetingServiceOuterClass.HelloResponse rs_cli = GreetingServiceOuterClass
+                .HelloResponse
+                .newBuilder()
+                .setGreeting("Работает!")
+                .build();
+        rs.onNext(rs_cli);
+        rs.onCompleted();
+    }
 }
